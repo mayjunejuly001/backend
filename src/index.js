c40c8c189d2277f771f1123c494b7bd399c28320
MAYANK VERMA <128578651+mayjunejuly001@users.noreply.github.com>
2025-03-10 11:06:50 +0530

    add user and video model

--- src/index.js
+++ src/index.js
@@ -7,4 +7,20 @@ dotenv.config({
 })
 
 connectDB()
+.then(() => {
+
+ app.on("erro" , (error) => {
+  console.log("Error:" , error);
+  throw error
+ })
+
+ app.listen(process.env.PORT || 8000 , () => {
+  console.log(`server is running at port : ${process.env.PORT} ` );
+  
+ })
+})
+.catch((err) => {
+ console.log("connection error " , err);
+ 
+})
 
